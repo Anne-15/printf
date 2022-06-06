54c7ae34a8642a9cb776b8110a71043c0e7e98cf
Anne-15 <wariiyuanne@gmail.com>
2022-06-06 03:44:11 -0700

    first project

--- main.h
+++ main.h
@@ -1,6 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct flags - struct containing flags
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/*printf*/
 int _printf(const char *format, ...);
 
 #endif
